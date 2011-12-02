7a798566a4db52d7527531a92339cc0a1f983d12
Cameron Lopez <cameron@cameron-macbook.local>
2011-12-02 10:51:37 -0600

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,16 @@
+require 'rbconfig'
+HOST_OS = RbConfig::CONFIG['host_os']
+source 'http://rubygems.org'
+gem 'rails', '3.1.3'
+gem 'sqlite3'
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+if HOST_OS =~ /linux/i
+  gem 'therubyracer', '>= 0.9.8'
+end
+gem "rspec-rails", ">= 2.8.0.rc1", :group => [:development, :test]
+gem "devise", ">= 1.5.0"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = '528caff9d1e59f791aac4cb07cf2c60fb0fc5da61cd0cc48111cb2783e3a111ba7dfe2222b5470e41c9f46d875f4319d4ed1f3f14ff201d5c9b1086b874ef723'
