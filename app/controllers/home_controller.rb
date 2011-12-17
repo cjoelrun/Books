f7dd711f4cffef5a98b8edab71f0c21aad67c767
Cameron Lopez <cameron@cameron-macbook.local>
2011-12-16 19:58:00 -0600

    fix index for unauthenticated user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @user = current_user
-    @books = @user.books
+    if @user
+      @books = @user.books
+    end
   end
 end
